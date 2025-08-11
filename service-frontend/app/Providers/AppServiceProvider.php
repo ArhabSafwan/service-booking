794c33d7a23a33cf9a080ca64f6fb39008097a2d
ArhabSafwan <asafwan72@gmail.com>
2025-08-11 22:31:28 +0600

    service frontend setup

--- service-frontend/app/Providers/AppServiceProvider.php
+++ service-frontend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+        DB::statement("SET time_zone = '+06:00'");
+        Schema::defaultStringLength(191);
     }
 }
